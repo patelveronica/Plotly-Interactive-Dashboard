c2469126fb0c94331893465e0e0799fb45b4c930
veronika patel <85206817+patelveronica@users.noreply.github.com>
2021-09-14 23:11:47 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-d3.json("samples1.json", function (d) {
+d3.json("samples.json", function (error, d) {
     console.log(d);
 });
-console.log("I like Ash and want to kiss him ");
+console.log("---");
