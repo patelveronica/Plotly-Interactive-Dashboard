648141d9af0e0b50f01bfe92fef80c6cb29a46b7
veronika patel <85206817+patelveronica@users.noreply.github.com>
2021-09-14 22:59:22 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,4 @@
-// d3.json("./StarterCode/samples.json", function (d) {
-//     console.log(d);
-// });
+d3.json("./StarterCode/samples.json", function (d) {
+    console.log(d);
+});
 console.log("I like Ash");
