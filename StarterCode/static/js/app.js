c48428d88d6e8db0e4f932fb7441bde742486cc4
veronika patel <85206817+patelveronica@users.noreply.github.com>
2021-09-15 19:52:59 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,3 +1,23 @@
-d3.json("samples.json").then(function(d) {
-    console.log(d)});
-console.log("-I love Data Science--");
+// read JSON file with d3
+d3.json("samples.json").then(function(data) {
+    console.log(data);
+    plotMetric(data)
+});
+
+function plotMetric(data){
+
+    let trace1 = {
+      x: data.sample_values,
+      y: data.otu_ids,
+      type: "bar"
+    }
+  
+    let plotdata = [trace1]
+  
+    // Pass metric to chart title
+    let layout = {
+      title: `give title later`
+    };
+  
+    Plotly.newPlot("plot",plotdata , layout);
+  }
