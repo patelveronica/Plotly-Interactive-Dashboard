1b8ee34bbca46c9c526eb209d260efe2e0325a14
veronika patel <85206817+patelveronica@users.noreply.github.com>
2021-09-15 21:03:41 -0400

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -2,19 +2,26 @@
 d3.json("samples.json").then(function(data) {
     console.log(data.samples);
     let id ="940"
+    let xArry = []
+    let yArray= []
 
     for (let i=0; i< data.samples.length; i++)
     {
         if (data.samples[i].id == id)
         {
-            console.log('v1');
-            console.log(data.samples[i])
+            console.log('v1');            
+          //  console.log(data.samples[i])
+          for (let j=0; j < 10 ; j++)
+          {
+            console.log(data.samples[i].otu_ids[j]);
+            xArry.push(data.samples[i].otu_ids[j])
+          }
+
         }
     }  
 
   //  plotMetric(data)
 });
-
 console.log('i like blue');
 
 
