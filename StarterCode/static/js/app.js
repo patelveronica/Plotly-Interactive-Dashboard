3972d52b8702a8601de981f9de934f2681e7fd90
patelveronica <85206817+patelveronica@users.noreply.github.com>
2021-09-20 16:49:12 -0400

    testing 4

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -6,19 +6,19 @@ d3.json("samples.json").then(function(data) {
 
 
 // get top 10 records
-var samplesvalues = samples.sample_value.slice(0, 10).reverse();
+var samplesvalues = newdata.sample_value.slice(0, 10).reverse();
 console.log(samplesvalues);
 
 // get top 10 OTU ids for plot
-var otu_topten = samples.otu_ids.slice(0, 10).reverse();
+var otu_topten = newdata.otu_ids.slice(0, 10).reverse();
 console.log(otu_topten);
 
 // get otu ids for plot
-var otu_ids = samples.map(id => "OTU" + id);
+var otu_ids = newdata.map(id => "OTU" + id);
 console.log(otu_ids);
 
 // get top 10 otu labels for plot
-var labels = samples.otu_labels.slice(0, 10);
+var labels = newdata.otu_labels.slice(0, 10);
 console.log(labels);
 });
 
