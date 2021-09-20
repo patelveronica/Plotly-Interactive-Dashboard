ebcf21ab305ace1905fb6048dcbd092cdfbd39a1
patelveronica <85206817+patelveronica@users.noreply.github.com>
2021-09-20 15:41:49 -0400

    testing2

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,30 @@
 d3.json("samples.json").then(function(data) {
-   Samples = (data.samples)
-   });
-console.log(Samples);
+   console.log(data.samples)
+
+//  filter sample values by ID
+var findId = data.samples.filter(object.id == id);
+let otudata = findId[0];
+console.log(otudata);
+
+// get top 10 records
+var samplesvalues = samples.sample_value.slice(0, 10).reverse();
+console.log(samplesvalues);
+
+// get top 10 OTU ids for plot
+var otu_topten = samples.otu_ids.slice(0, 10).reverse();
+console.log(otu_topten);
+
+// get otu ids for plot
+var otu_ids = samples.map(id => "OTU" + id);
+console.log(otu_ids);
+
+// get top 10 otu labels for plot
+var labels = samples.otu_labels.slice(0, 10);
+console.log(labels);
+});
+
+
+
 
    
    
